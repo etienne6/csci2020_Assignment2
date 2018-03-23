c20b76545215272c10eeecaee93a19386423e950
MirAfganTalpur <mir.talpur@uoit.net>
2018-03-23 15:48:25 -0400

    Added command line arguments, started on implementing ListView

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,20 +1,50 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Controller {
 
-    private ListView<sample.ProjectFile> localFiles = new ListView<>();
-    private ListView<ProjectFile> serverFiles = new ListView<>();
+    private ListView<String> localFiles = new ListView<>();
+    private ListView<String> serverFiles = new ListView<>();
     private Button downloadButton;
     private Button uploadButton;
+    private String sharedFolder;
+    List<String> fileList = new ArrayList<>();
+
+    public void setSharedFolder (String sharedFolder){ this.sharedFolder = sharedFolder; }
+
+    // tried to get the listview working, can't get it to display
+    public ListView<String> getFiles() {
+        File path = new File(sharedFolder);
+
+        File[] dirContents = path.listFiles();
+        List<String> fileList = new ArrayList<>();
+        for (int i = 0; i <= dirContents.length; i++) {
+            if (dirContents[i].isFile()) {
+                fileList.add(dirContents[i].getName());
+            }
+        }
+        ObservableList<String> files = FXCollections.observableArrayList(fileList);
+        ListView<String> localFiles = new ListView<String>(files);
+        localFiles.getItems().addAll(files);
+        return localFiles;
+    }
 
     public void OnDownload(ActionEvent actionEvent) {
     }
 
     public void OnUpload(ActionEvent actionEvent) {
     }
+
+
 }
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,19 +5,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.util.List;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
+        String computerName;
+        String sharedFolder;
+
+        // take in computer name and shared folder as command line arguments
+        Parameters params = getParameters();
+        List<String> parameters = params.getRaw();
+
+        computerName = parameters.get(0);
+        sharedFolder = parameters.get(1);
+
+        System.out.println(computerName);
+        System.out.println(sharedFolder);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("client.fxml"));
+        Parent root = (Parent)loader.load();
+        Controller controller = (Controller)loader.getController();
+        controller.setSharedFolder(sharedFolder);
+
         primaryStage.setTitle("File Sharer v1.0");
         primaryStage.setScene(new Scene(root, 550, 500));
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     }
 }
