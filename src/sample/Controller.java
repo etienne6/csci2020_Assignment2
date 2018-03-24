7125828c7713262911b0c1adfcaa9b02ad8e1466
MirAfganTalpur <mir.talpur@uoit.net>
2018-03-24 18:23:55 -0400

    Updated UI to show both sides of SplitPane, (server side still shows local folder right now)

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -18,7 +18,7 @@ public class Controller {
     @FXML
     private ListView<String> localFiles;
     @FXML
-    private ListView<String> serverFiles = new ListView<>();
+    private ListView<String> serverFiles;
     @FXML
     private Button downloadButton;
     @FXML
@@ -36,8 +36,12 @@ public class Controller {
         String sharedFolderPath = sharedFolder;
         // create Observable List of files from specified folder
         ObservableList<String> localFileList = getFiles(sharedFolderPath);
+        // create Observable List of files from specified folder
+        ObservableList<String> serverFilesList = getServerFiles(sharedFolderPath);
         // add observable list of files to ListView
         localFiles.setItems(localFileList);
+        // add observable list of files to ListView
+        serverFiles.setItems(localFileList);
     }
 
     // get files from specified folder and add to ListView
@@ -60,6 +64,28 @@ public class Controller {
         ObservableList<String> files = FXCollections.observableArrayList(fileList);
         return files;
         }
+
+    // get files from specified folder and add to ListView
+    public ObservableList<String> getServerFiles(String pathOfFiles) {
+        // use directory chooser to specify path
+        DirectoryChooser chooser = new DirectoryChooser();
+        File path = new File(pathOfFiles);
+        chooser.setInitialDirectory(path);
+        System.out.println(path);
+
+        // create array of files from path and add to array list
+        File[] filesInDirectory = path.listFiles();
+        List<String> fileList = new ArrayList<>();
+        for (int i = 0; i < filesInDirectory.length; i++) {
+            // check if content in path is a file, if it is, add to array list
+            if (filesInDirectory[i].isFile()) {
+                fileList.add(filesInDirectory[i].getName());
+            }
+        }
+        // create observable list from array list
+        ObservableList<String> files = FXCollections.observableArrayList(fileList);
+        return files;
+    }
 
     public void OnDownload(ActionEvent actionEvent) {
     }
